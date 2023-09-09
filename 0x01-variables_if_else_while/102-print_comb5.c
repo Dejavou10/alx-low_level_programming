1ef862cba6322aa4010aac28a3c877abe335350f
Dejavou10 <orolade@gmail.com>
2023-09-09 15:23:17 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,17 +13,20 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		if (i < j)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
-			putchar(' ');
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
-			if (i != 98 || j != 98)
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
